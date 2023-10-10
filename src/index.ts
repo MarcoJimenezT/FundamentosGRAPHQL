369645879ed87ce6f9757b101506adff89d21bd9
MarcoJimenez01 <sistemas3@americancotton.com.mx>
2023-10-09 19:38:40 -0600

    Revision de Practica

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+
+
+// Tipo query
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+
+type UserTypeDefinition {
+  name: String
+  email: String
+}  
+
+
+type Query {
+    users: [UserTypeDefinition],
+  }
+`;
+
+
+//Data Set Users 
+const users = [
+  {
+    name: "John",
+    email: "jhon@ejemplo.com"
+  },
+  {
+    name: "Miguel",
+    email: "miguel@ejemplo.com"
+  },
+  {
+    name: "Pedro",
+    email: "pedro@ejemplo.com"
+  },
+  {
+    name: "Balto",
+    email: "balto@ejemplo.com"
+  },
+]
+  
+
+// Resolvers
+
+const resolvers = {
+  Query: {
+    users: () => users[0], 
+  }
+}
+
+
+
+
+// SERVIDOR
+
+const server = new ApolloServer ({
+  typeDefs,
+  resolvers
+})
+
+async function iniciarApolloServer(){
+  const {url} = await startStandaloneServer(server,
+    {
+      listen: {
+        port:4000
+      }
+    }
+  )
+  
+  
+  console.log("App listening on ", url)
+}
+
